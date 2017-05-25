40bb9083fe9f9a9073a27f6d93abae79e04921a2
Aman Agarwal <aman15012@iiitd.ac.in>
2017-05-25 09:06:45 +0000

    i guess complete

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '06*j!1jf(*&x@#s029aneo#m#bbh-ea%o)q8xf*gpf)zsvxj6*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '192.168.57.77', '192.168.42.10','139.59.29.149','amanagarwal.xyz']
 
